be686357a0639c09d70e60f6119bf341a6c13e0d
HackPoint <genashm@gmail.com>
2016-04-17 01:01:36 +0300

    fix

--- typings/browser.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../source/interfaces/interfaces.d.ts" />
-/// <reference path="browser/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="browser/ambient/angular/index.d.ts" />
-/// <reference path="browser/ambient/chai/index.d.ts" />
-/// <reference path="browser/ambient/jquery/index.d.ts" />
-/// <reference path="browser/ambient/mocha/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../source/interfaces/interfaces.d.ts" />
-/// <reference path="main/ambient/angular-ui-router/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/chai/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
